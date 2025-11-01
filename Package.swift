0f77b76613385b2434f1ae70e0889f821d295743
Egor Egorov <egigoka@gmail.com>
2025-11-01 23:42:11 +0500

    fixed Package.swift to work with SPM

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "TimelineTableViewCell",
-    platforms: [.iOS(.v9)],
+    platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -22,7 +22,11 @@ let package = Package(
         .target(
             name: "TimelineTableViewCell",
             dependencies: [],
-            path: "TimelineTableViewCell"),
+            path: "TimelineTableViewCell",
+            resources: [
+                .process("TimelineTableViewCell.xib")
+            ]
+            ),
     ],
     swiftLanguageVersions: [.v5]
 )
